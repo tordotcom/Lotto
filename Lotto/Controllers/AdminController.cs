10f6587b3c250a60fb9f0b26e49fa1d40dfe725f
Arm <jrm.kaewsiri@gmail.com>
2019-05-30 00:16:58 +0700

    add all page

--- Lotto/Controllers/AdminController.cs
+++ Lotto/Controllers/AdminController.cs
@@ -9,7 +9,66 @@ namespace Lotto.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
-        public ActionResult Index()
+        public ActionResult Index() //หน้าแรก
+        {
+            return View();
+        }
+
+        public ActionResult AllBet() //ดูเลขทั้งหมด
+        {
+            return View();
+        }
+
+        public ActionResult Close() //ปิดหวย
+        {
+            return View();
+        }
+
+        public ActionResult List() //ดูโพย
+        {
+            return View();
+        }
+        public ActionResult Bet() //แทงโพย
+        {
+            return View();
+        }
+        public ActionResult BetTotal() //ยอดสรุปเป็นใบ
+        {
+            return View();
+        }
+        public ActionResult Out() //แทงออก
+        {
+            return View();
+        }
+        public ActionResult Cut() //ตัดเลข
+        {
+            return View();
+        }
+        public ActionResult OutTotal() //ยอดแทงออก
+        {
+            return View();
+        }
+        public ActionResult BetBackward() //ดูโพยย้อนหลัง
+        {
+            return View();
+        }
+        public ActionResult LottoBackward() //ดูหวยย้อนหลัง
+        {
+            return View();
+        }
+        public ActionResult Setting() //ตั้งค่า
+        {
+            return View();
+        }
+        public ActionResult Member() //สมาชิก
+        {
+            return View();
+        }
+        public ActionResult MemberPrice() //ราคาสมาชิก
+        {
+            return View();
+        }
+        public ActionResult DealerInfo() //ข้อมูลเจ้ามือ
         {
             return View();
         }
--- Lotto/Controllers/UserController.cs
+++ Lotto/Controllers/UserController.cs
@@ -9,13 +9,45 @@ namespace Lotto.Controllers
     public class UserController : Controller
     {
         // GET: User
-        public ActionResult Index()
+        public ActionResult Index() //หน้าแรก
         {
             return View();
         }
-        public ActionResult AddPoll()
+
+        public ActionResult Bet() //แทงโพย
         {
             return View();
         }
+
+        public ActionResult List() //ดูโพย
+        {
+            return View();
+        }
+
+        public ActionResult Return() //เลขคืน
+        {
+            return View();
+        }
+
+        public ActionResult Result() //ดูผล
+        {
+            return View();
+        }
+
+        public ActionResult Password() //รหัสผ่าน
+        {
+            return View();
+        }
+
+        public ActionResult Howto() //วิธีแทง
+        {
+            return View();
+        }
+
+        public ActionResult Contact() //ติดต่อเจ้ามือ
+        {
+            return View();
+        }
+
     }
 }
