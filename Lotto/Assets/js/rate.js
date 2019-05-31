67c6a8449583e615715464b8bc3154820e462329
witsanu <haallooweeen@gmail.com>
2019-06-01 01:37:50 +0700

    push get rate

---
+++ Lotto/Assets/js/rate.js
@@ -0,0 +1,31 @@
+﻿$("#btnRate").click(function () {
+    $.ajax({
+        url: getRate,
+        type: "POST",
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            $("#ThreeUp").val(data[0].ThreeUP);
+            $("#ThreeDown").val(data[0].ThreeDown);
+            $("#FirstThree").val(data[0].FirstThree);
+            $("#ThreeOod").val(data[0].ThreeOod);
+            $("#TwoUp").val(data[0].TwoUp);
+            $("#TwoOod").val(data[0].TwoOod);
+            $("#TwoDown").val(data[0].TwoDown);
+            $("#Up").val(data[0].Up);
+            $("#Down").val(data[0].Down);
+            $("#ThreeUp_discount").val(data[0].ThreeUP_discount);
+            $("#ThreeDown_discount").val(data[0].ThreeDown_discount);
+            $("#FirstThree_discount").val(data[0].FirstThree_discount);
+            $("#ThreeOod_discount").val(data[0].ThreeOod_discount);
+            $("#TwoUp_discount").val(data[0].TwoUp_discount);
+            $("#TwoOod_discount").val(data[0].TwoOod_discount);
+            $("#TwoDown_discount").val(data[0].TwoDown_discount);
+            $("#Up_discount").val(data[0].Up_discount);
+            $("#Down_discount").val(data[0].Down_discount);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert("error");
+        }
+    });
+});
---
+++ Lotto/Models/Rate_Discount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lotto.Models
+{
+    public class Rate_Discount
+    {
+        public string ThreeUP { get; set; }
+        public string ThreeDown { get; set; }
+        public string ThreeOod { get; set; }
+        public string FirstThree { get; set; }
+        public string TwoUp { get; set; }
+        public string TwoOod { get; set; }
+        public string TwoDown { get; set; }
+        public string Up { get; set; }
+        public string Down { get; set; }
+        public string ThreeUP_discount { get; set; }
+        public string ThreeDown_discount { get; set; }
+        public string ThreeOod_discount { get; set; }
+        public string FirstThree_discount { get; set; }
+        public string TwoUp_discount { get; set; }
+        public string TwoOod_discount { get; set; }
+        public string TwoDown_discount { get; set; }
+        public string Up_discount { get; set; }
+        public string Down_discount { get; set; }
+    }
+}
