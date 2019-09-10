63a9f6a5741f8603cbc0cd95a868b281d05e3207
witsanu <haallooweeen@gmail.com>
2019-09-10 20:02:46 +0700

    push txtname user

--- Lotto/Assets/js/member.js
+++ Lotto/Assets/js/member.js
@@ -1,44 +1,55 @@
 ﻿// Confirm Add && Update
 $("#btnEdit").click(function () {
-	if ($(this).val() == 1) {
-		//if ($('#editUserForm').valid()) {
-			var id = $(this).attr("data-id");
-			var UserData = {
-				ID: id,
-				Name: $("#txtName").val(),
-				Password: $("#txtPassword").val(),
-				Status: $("#txtStatusSelect").val(),
-				Description: $("#txtDescription").val()
-			};
-			$.ajax({
-				url: updateUser,
-				data: { User: UserData },
-				type: "POST",
-				dataType: "json",
-				success: function (data) {
-					if (data == "ss") {
-						Swal.fire({
-							type: 'success',
-							title: 'แก้ไขข้อมูลเรียบร้อย',
-							showConfirmButton: false,
-							timer: 1500
-						});
-						$('.modal-backdrop').remove();
-						$(".close").trigger('click');
-						setTimeout(function () {
-							location.reload(true);
-						}, 2000);
-					}
-				},
-				error: function (xhr, ajaxOptions, thrownError) {
-					alert("error");
-				}
-			});
+    if ($(this).val() == 1) {
+        //if ($('#editUserForm').valid()) {
+        var id = $(this).attr("data-id");
+        var UserData = {
+            ID: id,
+            Name: $("#txtName").val(),
+            Password: $("#txtPassword").val(),
+            Status: $("#txtStatusSelect").val(),
+            Description: $("#txtDescription").val()
+        };
+        if ($("#txtName").val() == "" || $("#txtPassword").val()=="") {
+            Swal.fire({
+                type: 'error',
+                title: 'กรุณากรอกชื่อสมาชิก',
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
+        else {
+            $.ajax({
+                url: updateUser,
+                data: { User: UserData },
+                type: "POST",
+                dataType: "json",
+                success: function (data) {
+                    if (data == "ss") {
+                        Swal.fire({
+                            type: 'success',
+                            title: 'แก้ไขข้อมูลเรียบร้อย',
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                        $('.modal-backdrop').remove();
+                        $(".close").trigger('click');
+                        setTimeout(function () {
+                            location.reload(true);
+                        }, 2000);
+                    }
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    alert("error");
+                }
+            });
+        }
 		//}
     }
 
 	if ($(this).val() == 0) {
 		//alert($("#txtUsername").val());
+        
         var AddUserData = {
 			Username: $("#txtUsername").val(),
             Name: $("#txtName").val(),
@@ -47,35 +58,46 @@ $("#btnEdit").click(function () {
             Description: $("#txtDescription").val()
 		};
 
-        $.ajax({
-            url: addUser,
-            data: { User: AddUserData },
-            type: "POST",
-            dataType: "json",
-            success: function (data) {
-                console.log(data);
-                if (data == "ss") {
-                    Swal.fire({
-                        type: 'success',
-                        title: 'บันทึกเรียบร้อย',
-                        showConfirmButton: false,
-                        timer: 1500
-                    });
-                    $('.modal-backdrop').remove();
-                    $(".close").trigger('click');
-                    setTimeout(function () {
-                        location.reload(true);
-                    }, 2000);
+        if ($("#txtName").val() == "" || $("#txtPassword").val() == "") {
+            Swal.fire({
+                type: 'error',
+                title: 'กรุณากรอกชื่อสมาชิก',
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
+        else {
+            $.ajax({
+                url: addUser,
+                data: { User: AddUserData },
+                type: "POST",
+                dataType: "json",
+                success: function (data) {
+                    console.log(data);
+                    if (data == "ss") {
+                        Swal.fire({
+                            type: 'success',
+                            title: 'บันทึกเรียบร้อย',
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                        $('.modal-backdrop').remove();
+                        $(".close").trigger('click');
+                        setTimeout(function () {
+                            location.reload(true);
+                        }, 2000);
+                    }
+                    else if (data == "dup") {
+                        Swal.fire('Username นี้มีผู้ใช้แล้ว');
+                    }
+                    else { }
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    alert("error");
                 }
-                else if (data == "dup") {
-                    Swal.fire('Username นี้มีผู้ใช้แล้ว');
-                }
-                else {}
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-                alert("error");
-            }
-        });
+            });
+        }
+        
     }
 });
 
